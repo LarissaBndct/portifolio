fce3b6a0bfdff5e05ece5dffd60b6c1cd2a39f9b
Larissa Benedicto <125518570+LarissaBndct@users.noreply.github.com>
2025-02-05 16:44:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+const btnEscuro = document.getElementById('btnEscuro');
+const btnClaro = document.getElementById('btnClaro');
+
+btnEscuro.addEventListener('click', function(){
+        alert("Escuro");
+});
+
+btnClaro.addEventListener('click', function(){
+        alert("claro");
+});
+    
